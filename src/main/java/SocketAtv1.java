741265e35cafb21e7e393177434ce1a64bd9c647
Unisul <Unisul@UNSO7DT98909.animaedu.intranet>
2025-09-15 11:06:42 -0300

    testCommit

--- src/main/java/SocketAtv1.java
+++ src/main/java/SocketAtv1.java
@@ -3,7 +3,7 @@ import java.io.PrintWriter;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.IOException;
-
+//test comment, hi
 public class SocketAtv1 {
     public static void main(String[] args) {
     try {
