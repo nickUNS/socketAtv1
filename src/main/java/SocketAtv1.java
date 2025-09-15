abe5fb710cf714667544ab11f1789b20feb332d5
Unisul <Unisul@UNSO7DT98909.animaedu.intranet>
2025-09-15 10:26:17 -0300

    Commit inicial

---
+++ src/main/java/SocketAtv1.java
@@ -0,0 +1,23 @@
+import java.net.Socket;
+import java.io.PrintWriter;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
+public class SocketAtv1 {
+    public static void main(String[] args) {
+    try {
+        Socket sock = new Socket("www.google.com.br", 80);
+        PrintWriter out = new PrintWriter(sock.getOutputStream(), true);
+        BufferedReader in = new BufferedReader(new InputStreamReader(sock.getInputStream()));
+        String linha = "";
+        out.println("GET / HTTP/1.0\n");
+        while ((linha = in.readLine()) != null) {
+            System.out.println("echo: " + linha);
+        }
+        System.out.println("Endereco e porta: " + sock.getRemoteSocketAddress());
+        } catch ( IOException e ) {
+            System.err.println( "Problemas de IO" );
+        }
+    }
+}
